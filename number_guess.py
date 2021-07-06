edc8e63425c73be162f6110e57a004264d83b13c
Alexrico05 <83270008+Alexrico05@users.noreply.github.com>
2021-07-06 17:42:48 +0200

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,44 @@
+#NumberGuess
+import random
+            
+def juego():
+    #Variables initialisation
+    solution = 0
+    y = 0
+
+    print "Hi welcome to the Number Guess!.I ll think a number and you wil have to try to guess it.First you ll have to give me the interval in which the number will be.Select the lower number"
+    lower_number = input()
+    print 'Now select the maximum number'
+    max_number = input()
+    print 'If you dont know the number enter: solution'
+
+    generated_number = random.randint(lower_number, max_number)
+
+    def attemps():
+        print 'Whats your choice?'
+        sele_num = input()
+    
+        if sele_num == generated_number:
+            print 'Congratulations you guessed the number!!'
+            print 'press y if you want ot play again'
+            restart = input()
+            if restart == y:
+                if __name__ == '__main__':
+                    juego()
+
+
+
+        elif sele_num == solution:
+            print generated_number
+
+        else:
+            print 'Ohh that was not the number!'
+            if __name__ == '__main__':
+                attemps()
+
+    if __name__ == '__main__':
+            attemps()
+
+        
+if __name__ == '__main__':
+            juego()
